6bc46bca02759e3c3fc89618a63cd760bae40369
hp1711 <harsh.phn@gmail.com>
2024-08-21 17:55:06 +0530

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+//First js program
+console.log("Hello World");
+
+//displaying date with use of inbuilt function
+console.log(Date());
+
+document.write("Hello World");
+
+//alert box
+//alert("Hello, welcome to the world of javascript");
+
+//variables
+
+let x, y, z;    
+x = 5;          
+y = 6;         
+z = x + y;
+
+let person = "John Doe";
+
+console.log(z);
+
+//re- declaring variable
+var carName = "Volvo";
+var carName="hi";
+console.log(carName);
+
+
+//string concatenation
+
+let h= "John" + " " + "Doe"; 
+console.log(h);
+
+let r = "5" + 2 + 3;
+console.log(r);
+
+let u = 2 + 3 + "5";
+console.log(u);
+
+let $myMoney = 5;
+console.log($myMoney);
+
